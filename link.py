419d53b85bb0f152a62316edd0d225044f1cce02
oranginisiald <130672062+oranginisiald@users.noreply.github.com>
2023-05-11 05:17:23 +0700

    Update link.py

--- link.py
+++ link.py
@@ -1,1 +1,7 @@
-
+import requests,json,os,time
+from requests import post
+from requests import get
+print ("\x1b[92m[\x1b[91m1\x1b[92m] \x1b[92m( Incloud Your Brain :v ) \x1b[91m:V")
+time.sleep (1)
+os.system("xdg-open https://youtube.com/channel/UC683ha2G4DEX24BjigEs7_w")
+time.sleep (8)
