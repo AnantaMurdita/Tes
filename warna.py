2c4d1af0f0a8833121b4dce5645f628414c7ee50
oranginisiald <130672062+oranginisiald@users.noreply.github.com>
2023-05-11 05:12:42 +0700

    Update warna.py

--- warna.py
+++ warna.py
@@ -1,1 +1,21 @@
+import requests,json,os,time
+from requests import post
+from requests import get
 
+#Warna
+red   = '\x1b[31m' # Merah
+green = '\x1b[32m' # Hijau
+yellow = '\x1b[33m' # Kuning
+blue  = '\x1b[34m' # Biru
+magenta = '\x1b[35m' # Ungu
+cyan  = '\x1b[36m' # Biru Muda
+white = '\x1b[37m' # Putih
+reset = '\x1b[39m' # Reset Warna ( Kembali Ke Warna Awal )
+brblack = '\x1b[90m' # Hitam Terang
+R = '\x1b[91m' # Merah Terang
+brgreen = '\x1b[92m' # Hijau Terang
+k = '\x1b[93m' # Kuning Terang
+brblue = '\x1b[94m' # Biru Terang
+brmgnt = '\x1b[95m' # Ungu Terang
+brcyan = '\x1b[96m' # Biru Muda Terang
+G = '\x1b[97m' # Putih Terang
