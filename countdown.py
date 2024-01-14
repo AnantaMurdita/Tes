2b30b198df99374b1df7acc8fb9dbc4430e5e33d
AnantaMurdita <127375283+AnantaMurdita@users.noreply.github.com>
2024-01-14 13:53:21 +0800

    Update countdown.py

--- countdown.py
+++ countdown.py
@@ -3,10 +3,10 @@ import time
 def countdown(time_sec):
     while time_sec:
         mins, secs = divmod(time_sec, 60)
-        timeformat = '{:02d}:{:02d}'.format(mins,secs)
+        timeformat = '{:02d}:{:02d}'.format(mins, secs)
         print("attack")
-        time.sleep(0.1)
-        time_sec -= 0.1
+        time.sleep(1)
+        time_sec -= 1
 
 # Contoh pemanggilan fungsi countdown
 countdown(100) 
