82fc7db9f4ad1d3269c78dda0be29aeb032e0182
AnantaMurdita <127375283+AnantaMurdita@users.noreply.github.com>
2024-01-14 13:47:02 +0800

    countdown.py

--- countdown.py
+++ countdown.py
@@ -4,9 +4,10 @@ def countdown(time_sec):
     while time_sec:
         mins, secs = divmod(time_sec, 60)
         timeformat = '{:02d}:{:02d}'.format(mins,secs)
-        print(timeformat,end='\r')
+        print("sabar 1 menit doang")
         time.sleep(1)
         time_sec -= 1
 
 # Contoh pemanggilan fungsi countdown
-countdown(60)  # Menghitung mundur selama 60 detik
+countdown(60) 
+# Menghitung mundur selama 60 detik
